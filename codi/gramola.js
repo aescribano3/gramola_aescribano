9d861b98555bb385eb032f65d12c43d2e984c80b
aescribano <aescribano@cendrassos.net>
2023-09-26 16:51:26 +0200

    Gramola_6

--- codi/gramola.js
+++ codi/gramola.js
@@ -24,6 +24,11 @@ function playAudio(){
     document.getElementById('play').src="../assets/img/pause.png";
     document.getElementById('play').removeEventListener('click', playAudio, true);
     document.getElementById('play').addEventListener('click', pauseAudio, true);
+    equalizer.style.display = "flex";
+    document.getElementById('random').src = "../assets/img/random.png";
+    document.getElementById('backward').src = "../assets/img/backward.png";
+    document.getElementById('forward').src = "../assets/img/forward.png";
+    document.getElementById('stop').src = "../assets/img/stop.png";
 }
 
 //Para y reinicia la canço, mostra boto de play
@@ -33,6 +38,7 @@ function stopAudio(){
     document.getElementById('play').src="../assets/img/play.png";
     document.getElementById('play').removeEventListener('click', pauseAudio, true);
     document.getElementById('play').addEventListener('click', playAudio, true);
+    equalizer.style.display = "none";
 }
 
 //Pausa la canço y mostra el boto de play
@@ -41,22 +47,72 @@ function pauseAudio(){
     document.getElementById('play').src="../assets/img/play.png";
     document.getElementById('play').removeEventListener('click', pauseAudio, true);
     document.getElementById('play').addEventListener('click', playAudio, true);
+    equalizer.style.display = "none";
 }
 audio = document.getElementById("audio");
 
-//Activa la opció de aeatori
-function randomAudio(){
+// Index de la canço actual
+var currentSongIndex = 0;
+
+// Ordre original
+var originalSongOrder = [];
+
+// Activa el random
+function randomAudio() {
+    // Si la lista original de canciones está vacía, llénala
+    if (originalSongOrder.length === 0) {
+        for (var i = 0; i < cançons.length; i++) {
+            originalSongOrder.push(i);
+        }
+    }
+
+    // Barreja l'ordre de les cançons
+    shuffleArray(originalSongOrder);
+
+    playSongAtIndex(originalSongOrder[currentSongIndex]);
 
 }
 
-//Avança a la seguent canço
-function backwardAudio(){
-
+// Avanza
+function forwardAudio() {
+    currentSongIndex++;
+    if (currentSongIndex >= cançons.length) {
+        currentSongIndex = 0;
+    }
+    playSongAtIndex(currentSongIndex);
 }
 
-//Retrocedir una canço
-function forwardAudio(){
-    
+// Retrocedeix
+function backwardAudio() {
+    currentSongIndex--;
+    if (currentSongIndex < 0) {
+        currentSongIndex = cançons.length - 1;
+    }
+    playSongAtIndex(currentSongIndex);
+}
+
+// Reproduir la canço en el index
+function playSongAtIndex(index) {
+    var song = cançons[index];
+    var audio = document.getElementById("audio");
+    audio.src = song.url;
+    audio.play();
+    document.getElementById('play').src = "../assets/img/pause.png";
+
+    // Actualitza la caratula
+    cover_img.src = song.cover;
+    cover_txt.textContent = song.title + " / " + song.artist;
+
+    // Actualitza el index
+    currentSongIndex = index;
+}
+
+// Barreja la playlist
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
 //Crida al play
@@ -106,14 +162,74 @@ function updateTimeDisplay(player) {
 var cover_img = document.getElementById("cover");
 var cover_txt = document.getElementById("cover_txt");
 
-// Función para reproducir una canción en el elemento de audio y actualizar la carátula y el título
+// Reprodueix la primera canço, afegeix les cancçons a la llista y modifica la caratula
 function playAudioLlista(url, title, cover, artist) {
     var audio = document.getElementById("audio");
     audio.src = url;
     audio.play();
-    document.getElementById('play').src="../assets/img/pause.png";
+    document.getElementById('play').src = "../assets/img/pause.png";
+    document.getElementById('random').src = "../assets/img/random.png";
+    document.getElementById('backward').src = "../assets/img/backward.png";
+    document.getElementById('forward').src = "../assets/img/forward.png";
+    document.getElementById('stop').src = "../assets/img/stop.png";
 
-    // Actualiza la carátula y el título
+    // Actualitza la caratula y el titol
     cover_img.src = cover;
     cover_txt.textContent = title + " / " + artist;
+    equalizer.style.display = "flex";
+}
+
+// Funció per moure la barra de temps
+function progress(event) {
+    const progressBar = document.getElementById('progress-bar');
+    const audio = document.getElementById('audio');
+
+    // Obtenir la posició
+    const clickX = event.clientX - progressBar.getBoundingClientRect().left;
+    const progressBarWidth = progressBar.clientWidth;
+    const progressPosition = (clickX / progressBarWidth) * audio.duration;
+
+    // Establir la nova posició
+    audio.currentTime = progressPosition;
+}
+
+// Actualitzar la barra mentres sona la canço
+audio.addEventListener('timeupdate', updateProgress);
+
+// Funció per actualitzar la barra
+function updateProgress() {
+    const audio = document.getElementById('audio');
+    const progress = document.getElementById('progress');
+    const progressBar = document.getElementById('progress-bar');
+
+    // Calcular progres
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
+    const progressWidth = (currentTime / duration) * 100;
+    progress.style.width = `${progressWidth}%`;
+}
+
+document.getElementById('progress-bar').addEventListener('click', progress);
+
+// Reprodueix la seguent canço
+audio.addEventListener('ended', playNextSong);
+
+// Función per reproduir la seguent canço
+function playNextSong() {
+    currentSongIndex++;
+    /*if (currentSongIndex >= cançons.length) {
+        currentSongIndex = 0;
+    }*/
+    playSongAtIndex(currentSongIndex);
+}
+
+audio.addEventListener('ended', playNextSong);
+
+if (audio.src == ""){
+    equalizer.style.display = "none";
+    document.getElementById('random').src = "../assets/img/random_off.png";
+    document.getElementById('backward').src = "../assets/img/backward_off.png";
+    document.getElementById('play').src = "../assets/img/play_off.png";
+    document.getElementById('forward').src = "../assets/img/forward_off.png";
+    document.getElementById('stop').src = "../assets/img/stop_off.png";
 }
--- codi/index.php
+++ codi/index.php
@@ -9,6 +9,16 @@ if (isset($_GET["playlist_id"])) {
 $data = file_get_contents($files[$playlistId]);
 $playlist = json_decode($data, true);
 $cançons = $playlist["cançons"];
+
+// Verifica si s'ha iniciat sessio
+$loggedIn = false;
+
+if (isset($_POST["name"]) && isset($_POST["pwd"])) {
+    $username = $_POST["name"];
+    $password = $_POST["pwd"];
+    $loggedIn = true;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,14 +29,29 @@ $cançons = $playlist["cançons"];
         <link rel="icon" type="image/png" href="../assets/img/icono.PNG">
         <!--Link de l'estil css-->
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Wavefont">
         <link rel="stylesheet" href="gramola.css">
         <title>GRAMOLA / AESCRIBANO</title>
     </head>
     <body>
-        <header>
-            <!--Icone de la pagina-->
-            <img id="icono" src="../assets/img/icono.PNG" alt="icono">
-        </header>
+    <header>
+        <!-- Icone de la pagina -->
+        <img id="icono" src="../assets/img/icono.PNG" alt="icono">
+        <?php if ($loggedIn): ?>
+        <!-- Mostra el nom y el logout si s'ha iniciat sessió -->
+        <div id="user-info">
+            <h2 id="nom"><?php echo $username ?></h2>
+            <a href="index.php"><h4 id="logout">Log Out</h4></a>
+        </div>
+        <?php else: ?>
+        <!-- Monstra formulari si no s'ha iniciat sessió -->
+        <form id="login-form" action="index.php" method="post">
+            <input type="text" id="name" name="name" placeholder="Nom d'usuari" required>
+            <input type="password" id="pwd" name="pwd" placeholder="Contrasenya" required>
+            <input type="submit" value="Enviar">
+        </form>
+        <?php endif; ?>
+    </header>
         <div id="casete">
         <!-- Afegeix un enllaç a les imatges -->
         <a href="index.php?playlist_id=0"><img id='Classic' class="Slider" src="../assets/img/casete_classic.png"></a>
@@ -62,6 +87,16 @@ $cançons = $playlist["cançons"];
         </div>
         <footer>
             <!--Creació del control d'audio-->
+            <div class="equalizer" id="equalizer">
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+                <div class="equalizer__bar"></div>
+            </div>
             <img id="random" src="../assets/img/random.png">
             <img id="backward" src="../assets/img/backward.png">
             <img id="play" src="../assets/img/play.png">
@@ -70,11 +105,24 @@ $cançons = $playlist["cançons"];
             <audio id="audio" ontimeupdate="updateTimeDisplay(this)">
                 <source src="" type="audio/mp3">
             </audio>
-            <div id="progress-bar">
+            <div id="progress-bar" onclick="progress(event)">
                 <div id="progress"></div>
             </div>
             <div id="duration">0:00 / 0:00</div>
             <script src="gramola.js"></script>
         </footer>
+        <script>
+            // Verifica si s'ha especificat playlist_id a la URL
+            const urlParams = new URLSearchParams(window.location.search);
+            const playlistId = urlParams.get("playlist_id");
+
+            // Si playlist_id esta definit, reprodueix la primera canço de la llista
+            if (playlistId !== null && !isNaN(playlistId) && playlistId >= 0 && playlistId < <?php echo count($files); ?>) {
+                const firstSong = <?php echo json_encode($playlist["cançons"][0]); ?>;
+                playAudioLlista(firstSong.url, firstSong.title, firstSong.cover, firstSong.artist);
+            }
+
+            var cançons = <?php echo json_encode($cançons); ?>;
+        </script>
     </body>
 </html>
